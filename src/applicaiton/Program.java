a3d219f066d783426c046a898e03da31f61675b0
BraianMelo <Braian.ptu@gmail.com>
2019-08-22 21:34:25 -0300

    Seller entities class

--- src/applicaiton/Program.java
+++ src/applicaiton/Program.java
@@ -1,12 +1,16 @@
 package applicaiton;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		Seller s = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000, obj);
+		System.out.println(s);
 
 	}
 
